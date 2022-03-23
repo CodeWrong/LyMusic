6bf41252ac01beab0c576783e87b7a59cf3a5d28
“Roly” <493782517@qq.com>
2022-03-23 22:06:51 +0800

    搜索关键字高亮显示

--- pages/detail-search/index.js
+++ pages/detail-search/index.js
@@ -5,14 +5,14 @@ import debounce from '../../utils/debounce';
 const debounceGetSearchSuggest = debounce(getSearchSuggest,300);
 
 Page({
-
     /**
      * 页面的初始数据
      */
     data: {
         hotKeyWords: [],
         suggestSongs: [],
-        searchValue: ''
+        searchValue: '',
+        nodes: []
     },
 
     /**
@@ -33,7 +33,61 @@ Page({
         this.setData({searchValue: searchValue})
         if(searchValue){
             debounceGetSearchSuggest(searchValue).then(res => {
-                this.setData({suggestSongs: res.result.allMatch})
+                const suggestSongs = res.result.allMatch
+                this.setData({suggestSongs})
+
+                // 转为nodes节点
+                const suggestKeywords = suggestSongs.map(item => item.keyword);
+                const suggestNodes = []
+                for(const keyword of suggestKeywords){
+                    const nodes = [];
+                    if(keyword.toUpperCase().startsWith(searchValue.toUpperCase())){
+                        const key1 = keyword.slice(0, searchValue.length);
+                        const node1 = {
+                            name: 'span',
+                            attrs: {
+                                style: "color: #26ce8a"
+                            },
+                            children: [
+                                {
+                                    type: "text",
+                                    text: key1
+                                }
+                            ]
+                        };
+                        nodes.push(node1);
+                        const key2 = keyword.slice(searchValue.length);
+                        const node2 = {
+                            name: 'span',
+                            attrs: {
+                                style: "color: black"
+                            },
+                            children: [
+                                {
+                                    type: "text",
+                                    text: key2
+                                }
+                            ]
+                        };
+                        nodes.push(node2);
+                    }else{
+                        const node1 = {
+                            name: 'span',
+                            attrs: {
+                                style: "color: #000"
+                            },
+                            children: [
+                                {
+                                    type: "text",
+                                    text: keyword
+                                }
+                            ]
+                        };
+                        nodes.push(node1);
+                    }
+                    suggestNodes.push(nodes)
+                }
+                this.setData({nodes: suggestNodes})
             });
         }else{
             this.setData({suggestSongs: []})
