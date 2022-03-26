b42ad77b0a361721ea0a8fa0946a07b704e77af0
“Roly” <493782517@qq.com>
2022-03-26 11:12:38 +0800

    fix: 整体的分页布局

--- pages/music-player/index.js
+++ pages/music-player/index.js
@@ -7,7 +7,10 @@ Page({
      */
     data: {
         id: 0,
-        currentSong: {}
+        currentSong: {},
+
+        currentPage: 0,
+        contentHeight: 0
     },
 
     /**
@@ -19,13 +22,27 @@ Page({
         this.setData({id})
         // 根据id获取歌曲信息
         this.getPageData(id)
+
+        // 动态计算内容高度
+        const globalData = getApp().globalData;
+        
+        const screenHeight = globalData.screenHeight;
+        const statusBarHeight = globalData.statusBarHeight;
+        const navBarHeight = globalData.navBarHeight;
+        const contentHeight = screenHeight - statusBarHeight - 44;
+        this.setData({contentHeight})
     },
 
     getPageData: function(id) {
         getSongDetail(id).then(res => {
-            console.log(res)
             this.setData({currentSong: res.songs[0]})
         })
+    },
+
+    // 事件处理
+    handleSwiperChange: function(event){
+        const current = event.detail.current;
+        this.setData({currentPage: current})
     },
 
     /**
