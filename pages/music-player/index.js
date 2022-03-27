7cc659328eabed0b55286133338e82e34cc918cf
“Roly” <493782517@qq.com>
2022-03-27 15:15:12 +0800

    feat: 播放页面滚动设置

--- pages/music-player/index.js
+++ pages/music-player/index.js
@@ -1,7 +1,14 @@
 // pages/music-player/index.js
-import {getSongDetail, getSongLyric} from '../../service/api_player';
-import {audioContext} from '../../store/index'
-import {parseLyric} from '../../utils/parse-lyric';
+import {
+    getSongDetail,
+    getSongLyric
+} from '../../service/api_player';
+import {
+    audioContext
+} from '../../store/index'
+import {
+    parseLyric
+} from '../../utils/parse-lyric';
 
 
 Page({
@@ -20,10 +27,11 @@ Page({
 
         // 是否显示歌词
         isMusicLyric: true,
-        currentTime:0,
+        currentTime: 0,
         durationTime: 0,
         sliderValue: 0,
-        isSliderChanging: false
+        isSliderChanging: false,
+        lyricScrollTop: 0
     },
 
     /**
@@ -32,20 +40,26 @@ Page({
     onLoad: function (options) {
         // 获取传入的id
         const id = options.id;
-        this.setData({id})
+        this.setData({
+            id
+        })
         // 根据id获取歌曲信息
         this.getPageData(id)
 
         // 动态计算内容高度
         const globalData = getApp().globalData;
-        
+
         const screenHeight = globalData.screenHeight;
         const statusBarHeight = globalData.statusBarHeight;
         const navBarHeight = globalData.navBarHeight;
         const deviceRadio = globalData.deviceRadio;
         const contentHeight = screenHeight - statusBarHeight - navBarHeight;
-        this.setData({contentHeight});
-        this.setData({isMusicLyric: deviceRadio >= 2});
+        this.setData({
+            contentHeight
+        });
+        this.setData({
+            isMusicLyric: deviceRadio >= 2
+        });
 
 
         // 使用音乐播放器
@@ -55,51 +69,65 @@ Page({
 
         this.setupAudioContextListener();
     },
-    setupAudioContextListener(){
+    setupAudioContextListener() {
         audioContext.onCanplay(() => {
             // audioContext.play();
         })
 
         audioContext.onTimeUpdate(() => {
             const currentTime = audioContext.currentTime * 1000;
-            if(!this.data.isSliderChanging){
+            if (!this.data.isSliderChanging) {
                 const value = currentTime / this.data.durationTime * 100
-                this.setData({currentTime,sliderValue: value})
+                this.setData({
+                    currentTime,
+                    sliderValue: value
+                })
             }
             let i = 0
-            for(; i < this.data.lyricInfos.length;i++){
+            for (; i < this.data.lyricInfos.length; i++) {
                 const lyricInfo = this.data.lyricInfos[i];
-                if(currentTime < lyricInfo.time){
+                if (currentTime < lyricInfo.time) {
                     break;
                 }
             }
             const currentIndex = i - 1;
-            if(this.data.currentLyricIndex !== currentIndex){
+            if (this.data.currentLyricIndex !== currentIndex) {
                 const currentLyricInfo = this.data.lyricInfos[currentIndex];
-                this.setData({currentLyricText: currentLyricInfo.lyricText,currentLyricIndex: currentIndex})
+                this.setData({
+                    currentLyricText: currentLyricInfo.lyricText,
+                    currentLyricIndex: currentIndex,
+                    lyricScrollTop: currentIndex * 35
+                })
             }
         })
     },
 
-    getPageData: function(id) {
+    getPageData: function (id) {
         getSongDetail(id).then(res => {
-            this.setData({currentSong: res.songs[0], durationTime: res.songs[0].dt})
+            this.setData({
+                currentSong: res.songs[0],
+                durationTime: res.songs[0].dt
+            })
         })
 
         getSongLyric(id).then(res => {
             const lyricString = res.lrc.lyric;
             const lyrics = parseLyric(lyricString);
-            this.setData({lyricInfos: lyrics})
+            this.setData({
+                lyricInfos: lyrics
+            })
         })
     },
 
     // 事件处理
-    handleSwiperChange: function(event){
+    handleSwiperChange: function (event) {
         const current = event.detail.current;
-        this.setData({currentPage: current})
+        this.setData({
+            currentPage: current
+        })
     },
 
-    handleSliderChange: function(event){
+    handleSliderChange: function (event) {
         // 获取slider变化的值
         const value = event.detail.value;
 
@@ -108,13 +136,20 @@ Page({
         audioContext.pause();
         audioContext.seek(currentTime / 1000);
 
-        this.setData({sliderValue: value,isSliderChanging: false})
+        this.setData({
+            sliderValue: value,
+            isSliderChanging: false
+        })
     },
 
-    handleSliderChanging(event){
+    handleSliderChanging(event) {
         const value = event.detail.value;
         const currentTime = this.data.durationTime * value / 100;
-        this.setData({isSliderChanging: true, currentTime, sliderValue: value})
+        this.setData({
+            isSliderChanging: true,
+            currentTime,
+            sliderValue: value
+        })
     },
 
     /**
